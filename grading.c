91696317bf0cbc36d0e2328b2d26ace17d727f48
anantec <asingh1037125@gmail.com>
2024-08-24 15:14:59 +0530

    grade code change

--- grading.c
+++ grading.c
@@ -2,16 +2,16 @@
 
 
 char calculate_grade(int marks) {
-    if (marks >= 90) {
+    if (marks > 85 && marks<=100) {
         return 'A';
-    } else if (marks >= 80) {
+    } else if (marks > 60 && marks <= 85) {
+        printf("B+");
+    } else if (marks > 40 && marks <= 60 ){
         return 'B';
-    } else if (marks >= 70) {
+    } else if (marks > 30 && marks <= 40) {
         return 'C';
-    } else if (marks >= 60) {
-        return 'D';
     } else {
-        return 'F';
+        printf("Sorry you are fail");
     }
 }
 
@@ -21,7 +21,7 @@ int main() {
     scanf("%d", &marks);
 
     char grade = calculate_grade(marks);
-    printf("Grade: %c\n", grade);
+    printf("\nGrade: %c\n", grade);
 
     return 0;
 }
